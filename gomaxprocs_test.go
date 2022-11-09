04d8bcb01271f44d76c63c3c9fe436fce4edc46f
paul_lestyo <paulus.lestyo@student.uns.ac.id>
2022-11-10 02:02:06 +0700

    first commit

---
+++ gomaxprocs_test.go
@@ -0,0 +1,56 @@
+package belajar_golang_goroutines
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestGetGomaxprocs(t *testing.T) {
+	group := sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer group.Done()
+			group.Add(1)
+			time.Sleep(3 * time.Second)
+		}()
+	}
+
+	totalCpu := runtime.NumCPU()
+	fmt.Println("Total CPU", totalCpu)
+
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Total Thread", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Total Goroutine", totalGoroutine)
+
+	group.Wait()
+}
+
+func TestChangeGomaxprocs(t *testing.T) {
+	group := sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer group.Done()
+			group.Add(1)
+			time.Sleep(3 * time.Second)
+		}()
+	}
+
+	totalCpu := runtime.NumCPU()
+	fmt.Println("Total CPU", totalCpu)
+
+	runtime.GOMAXPROCS(20)
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Total Thread", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Total Goroutine", totalGoroutine)
+
+	group.Wait()
+}
